148004e64f9132ed8d4b96f9f57674a069c01f29
wujinhjun <90538413+wujinhjun@users.noreply.github.com>
2022-05-28 10:30:24 +0800

    feat: init the repo and function

---
+++ py/server.py
@@ -0,0 +1,58 @@
+import asyncio
+
+import websockets
+
+userName = 'wujinhjun'
+userPassword = "kaimozhenhao"
+
+
+# 检测客户端权限，用户名密码通过才能退出循环
+async def check_permit(websocket):
+    while True:
+        recv_str = await websocket.recv()
+        cred_dict = recv_str.split(":")
+        if cred_dict[0] == userName and cred_dict[1] == userPassword:
+            response_str = "已连接到服务器\r\n可以开始发送信息"
+            await websocket.send(response_str)
+            return True
+        else:
+            response_str = "认证失败"
+            await websocket.send(response_str)
+
+
+# 接收客户端消息并处理，这里只是简单把客户端发来的返回回去
+async def recv_msg(websocket):
+    while True:
+        recv_text = await websocket.recv()
+        response_text = f"服务器收到: {recv_text}"
+        await websocket.send(response_text)
+
+
+# 服务器端主逻辑
+# websocket和path是该函数被回调时自动传过来的，不需要自己传
+async def main_logic(websocket, path):
+    await check_permit(websocket)
+
+    await recv_msg(websocket)
+
+
+def main():
+    start_server = websockets.serve(main_logic, 'localhost', 5678)
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
+
+
+if __name__ == "__main__":
+    main()
+
+# # 把ip换成自己本地的ip
+# start_server = websockets.serve(main_logic, 'localhost', 5678)
+# # 如果要给被回调的main_logic传递自定义参数，可使用以下形式
+# # 一、修改回调形式
+# # import functools
+# # start_server = websockets.serve(functools.partial(main_logic, other_param="test_value"), '10.10.6.91', 5678)
+# # 修改被回调函数定义，增加相应参数
+# # async def main_logic(websocket, path, other_param)
+#
+# asyncio.get_event_loop().run_until_complete(start_server)
+# asyncio.get_event_loop().run_forever()
---
+++ webPage/js/script.js
@@ -0,0 +1,85 @@
+// 打开一个 web socket
+const ws = new WebSocket("ws://localhost:5678");
+const sendButton = document.getElementById('send-btn');
+
+function verify() {
+    ws.onopen = function () {
+        ws.send("wujinhjun:kaimozhenhao");
+        alert("正在验证");
+    }
+}
+
+function sendMessage(message) {
+    ws.onopen = function () {
+        ws.send(`${message}`);
+        console.log('send successful');
+    }
+}
+
+function receive() {
+    ws.onmessage = function (evt) {
+        let received_msg = evt.data;
+        if (received_msg.indexOf("sorry") === -1) {
+            alert("收到信息： " + received_msg);
+        }
+    }
+}
+
+function closeCon() {
+    ws.onclose = function () {
+        alert("连接已关闭...");
+    }
+}
+
+function webSocketTest() {
+    if ("WebSocket" in window) {
+
+        // 连接建立后的回调函数
+        verify();
+
+        // 接收到服务器消息后的回调函数
+        receive();
+
+        // 连接关闭后的回调函数
+        closeCon();
+
+
+        // //发送信息
+        const msg = textArea();
+        controlMessage(msg);
+    } else {
+        // 浏览器不支持 WebSocket
+        alert("您的浏览器不支持 WebSocket!");
+    }
+}
+
+function textArea() {
+    const theTextContent = document.getElementById('text-scope');
+    const msg = theTextContent.value;
+    // console.log(msg);
+    return msg;
+}
+
+function clearText() {
+    const theTextContent = document.getElementById('text-scope');
+    // console.log(theTextContent);
+    theTextContent.value = '';
+    // theTextContent.textContent = " ";
+    // console.log('clear');
+}
+
+
+function controlMessage(message) {
+    sendButton.onclick = () => {
+        const msg = textArea();
+        // sendMessage(msg);
+        ws.send(msg);
+        clearText();
+    }
+}
+
+// WebSocketTest();
+webSocketTest();
+
+
+
